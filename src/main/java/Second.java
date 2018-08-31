03ae8c6bae4cb535126457251e6ae45044a156c5
damianjurek1985 <damianjurek85@gmail.com>
2018-08-31 11:46:16 +0200

    4 commit

--- src/main/java/Second.java
+++ src/main/java/Second.java
@@ -1,4 +1,11 @@
 public class Second {
     //new class
     //new line
+    //new line2
+    //new line3
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
